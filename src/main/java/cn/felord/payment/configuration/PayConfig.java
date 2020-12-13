2b31a7f18b6808889c3456d7ae24d37bc5b20665
felord.cn <dax@felord.cn>
2020-12-13 13:52:11 +0800

    配置

--- src/main/java/cn/felord/payment/configuration/PayConfig.java
+++ src/main/java/cn/felord/payment/configuration/PayConfig.java
@@ -1,5 +1,9 @@
 package cn.felord.payment.configuration;
 
+import cn.felord.payment.autoconfigure.EnableMobilePay;
+import org.springframework.context.annotation.Configuration;
 
+@EnableMobilePay
+@Configuration
 public class PayConfig {
 }
