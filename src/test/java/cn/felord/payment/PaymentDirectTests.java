ba37128b8a915ba95f617be972382e2fe7eff47e
dax <felord@qq.com>
2020-12-16 14:15:26 +0800

    直连支付test

--- src/test/java/cn/felord/payment/PaymentDirectTests.java
+++ src/test/java/cn/felord/payment/PaymentDirectTests.java
@@ -6,12 +6,13 @@ import cn.felord.payment.wechat.v3.model.Amount;
 import cn.felord.payment.wechat.v3.model.PayParams;
 import cn.felord.payment.wechat.v3.model.Payer;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
- * The type Payment direct tests.
+ * 支付直连模式测试.
  *
  * @author Dax
  * @since 13 :39
@@ -43,7 +44,11 @@ public class PaymentDirectTests {
         amount.setTotal(100);
         payParams.setAmount(amount);
         WechatResponseEntity<ObjectNode> responseEntity = wechatApiProvider.directPayApi(tenantId).appPay(payParams);
+
+        Assertions.assertThat(responseEntity.is2xxSuccessful()).isTrue();
         // responseEntity = WechatResponseEntity(httpStatus=200, body={"prepay_id":"wx1613461177695369fdbcfbd5ba8d0f0000"})
+        Assertions.assertThat(responseEntity.getBody().get("prepay_id").asText()).isNotBlank();
+
     }
 
     /**
@@ -65,7 +70,9 @@ public class PaymentDirectTests {
         payParams.setPayer(payer);
 
         WechatResponseEntity<ObjectNode> responseEntity = wechatApiProvider.directPayApi(tenantId).jsPay(payParams);
+        Assertions.assertThat(responseEntity.is2xxSuccessful()).isTrue();
 // responseEntity = WechatResponseEntity(httpStatus=200, body={"prepay_id":"wx16140503583504b53b0ddcd64cc2430000"})
+        Assertions.assertThat(responseEntity.getBody().get("prepay_id").asText()).isNotBlank();
     }
 
     /**
@@ -82,7 +89,9 @@ public class PaymentDirectTests {
         amount.setTotal(100);
         payParams.setAmount(amount);
         WechatResponseEntity<ObjectNode> responseEntity = wechatApiProvider.directPayApi(tenantId).nativePay(payParams);
+        Assertions.assertThat(responseEntity.is2xxSuccessful()).isTrue();
         // responseEntity ---->  WechatResponseEntity(httpStatus=200, body={"code_url":"weixin://wxpay/bizpayurl?pr=R4KZBgV00"})
+        Assertions.assertThat(responseEntity.getBody().get("code_url").asText()).isNotBlank();
     }
 
     // 其它不再举例说明 结合微信文档进行处理。
